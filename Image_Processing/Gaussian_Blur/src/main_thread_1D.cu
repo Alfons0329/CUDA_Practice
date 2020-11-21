e8d34a83f8c2cbf09e6e8cd56aa0591e0977b1ec
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-11-21 15:49:59 +0800

    Finished 1D version

--- Image_Processing/Gaussian_Blur/src/main_thread_1D.cu
+++ Image_Processing/Gaussian_Blur/src/main_thread_1D.cu
@@ -15,7 +15,7 @@
 using namespace std;
 
 // CUDA Stream
-#define N_STREAMS 8
+#define N_STREAMS 20
 
 // Gaussian filter
 int filter_size;
@@ -28,10 +28,19 @@ unsigned char *img_output;
 
 // Global data
 int thread_cnt, block_row;
+int cudaError_cnt;
 string img_name;
 
+// CUDA error checker
+void cuda_err_chk(const cudaError_t& e, const int& cudaError_cnt){
+    if(e != cudaSuccess){
+        fprintf(stderr, "cudaError in no. %d: %s\n", cudaError_cnt, cudaGetErrorString(e));
+        exit(EXIT_FAILURE);
+    }
+}
+
 // Kernel, 1D dim and grid configuration version
-__global__ void cuda_gaussian_filter_block_1D(unsigned char* img_input_cuda, unsigned char* img_output_cuda,int img_col, int img_row, int shift, unsigned int* filter_cuda, int filter_row, unsigned int filter_scale, int img_border){
+__global__ void cuda_gaussian_filter_thread_1D(unsigned char* img_input_cuda, unsigned char* img_output_cuda, int img_row, int img_col, int shift, unsigned int* filter_cuda, int filter_row, unsigned int filter_scale, int img_border){
     int cuda_offset = blockIdx.x * blockDim.x + threadIdx.x;
     int cuda_row = cuda_offset / img_row;
     int cuda_col = cuda_offset % img_col;
@@ -54,7 +63,6 @@ __global__ void cuda_gaussian_filter_block_1D(unsigned char* img_input_cuda, uns
             if (target >= img_border || target < 0){
                 continue;
             }
-            
 			tmp += filter_cuda[j * filter_row + i] * img_input_cuda[target];  
         }
     }
@@ -67,28 +75,12 @@ __global__ void cuda_gaussian_filter_block_1D(unsigned char* img_input_cuda, uns
     img_output_cuda[3 * (cuda_row * img_col + cuda_col) + shift] = tmp;
 }
 
-int cuda_run(const int& async){
+int cuda_run(const int& img_row, const int& img_col, const int& resolution, const int& async){
     /*-------------- CUDA init ------------*/
-    int num = 0;
-    cudaGetDeviceCount(&num);
-    cudaDeviceProp prop;
-    if(num > 0){
-        cudaGetDeviceProperties(&prop, 0);
-        printf("Device: %s \n", prop.name.c_str());
-    }
-    else{
-        fprintf(stderr, "%s", "No NVIDIA GPU detected!\n");
-        return 1;
-    }
+    // Allocate memory
+    img_output = new unsigned char[resolution];
+    memset(img_output, 0, sizeof(unsigned char) * resolution);
 
-    // Init CUDA streams
-    cudaStream_t streams[N_STREAMS];
-    for (int i = 0; i < N_STREAMS; i++) {
-        cudaStreamCreate(&streams[i]);
-    }
-    int offset = 0;
-    int chunk_size = resolution / N_STREAMS;
-    
     unsigned char* img_input_cuda;
     unsigned char* img_output_cuda;
     unsigned int* filter_cuda;
@@ -101,13 +93,22 @@ int cuda_run(const int& async){
     
     // Thread configurations
     const dim3 block_size(thread_cnt);
-    const dim3 grid_size((resolution / N_STREAMS + thread_cnt - 1) / thread_cnt);
+    const dim3 grid_size_sync((resolution + thread_cnt - 1) / thread_cnt);
+    const dim3 grid_size_async((resolution / N_STREAMS + thread_cnt - 1) / thread_cnt);
     
+    // Init CUDA streams
+    int offset = 0;
+    int chunk_size = resolution / N_STREAMS;
+    cudaStream_t streams[N_STREAMS];
+    for (int i = 0; i < N_STREAMS; i++) {
+        cudaStreamCreate(&streams[i]);
+    }
+
+    // Measuring time
+    struct timeval start, end; 
+    gettimeofday(&start, 0);
     if(async){
         /*-------------- CUDA run async ------------*/
-        // R G B channel respectively
-        struct timeval start, end; 
-        gettimeofday(&start, 0);
         for(int j = 0; j < N_STREAMS; j++){
             offset = chunk_size * j;
             
@@ -115,67 +116,106 @@ int cuda_run(const int& async){
             cuda_err_chk(cudaMemcpyAsync(img_output_cuda + offset, img_output + offset, chunk_size * sizeof(unsigned char), cudaMemcpyHostToDevice, streams[j]), cudaError_cnt++);
             
             for(int i = 0; i < 3; i++) {
-                cuda_gaussian_filter_block_1D<<<grid_size, block_size, 0, streams[j]>>>(img_input_cuda + offset, img_output_cuda + offset, img_col, img_row, i, filter_cuda, filter_row, filter_scale, chunk_size); 
+                cuda_gaussian_filter_thread_1D<<<grid_size_async, block_size, 0, streams[j]>>>(img_input_cuda + offset, img_output_cuda + offset, img_row, img_col, i, filter_cuda, filter_row, filter_scale, chunk_size); 
             }
             
             cuda_err_chk(cudaMemcpyAsync(img_output + offset, img_output_cuda + offset, chunk_size * sizeof(unsigned char), cudaMemcpyDeviceToHost, streams[j]), cudaError_cnt++);
         }
-    
-        // cuda_err_chk(cudaDeviceSynchronize(), cudaError_cnt++);
     }
     else{
-        
+        /*-------------- CUDA run sync ------------*/
+        cuda_err_chk(cudaMemcpy(img_input_cuda, img_input, resolution * sizeof(unsigned char), cudaMemcpyHostToDevice), cudaError_cnt++);
+        for(int i = 0; i < 3; i++) {
+            cuda_gaussian_filter_thread_1D<<<grid_size_sync, block_size>>>(img_input_cuda, img_output_cuda, img_row, img_col, i, filter_cuda, filter_row, filter_scale, resolution);
+        }
+        cuda_err_chk(cudaMemcpy(img_output, img_output_cuda, resolution * sizeof(unsigned char), cudaMemcpyDeviceToHost), cudaError_cnt++);
     }
+    // cuda_err_chk(cudaDeviceSynchronize(), cudaError_cnt++);
+
     gettimeofday(&end, 0);
     int sec = end.tv_sec - start.tv_sec;
     int usec = end.tv_usec - start.tv_usec;
     int t_gpu = sec * 1000 + (usec / 1000);
     printf(ANSI_COLOR_RED "GPU time (ms): %d " ANSI_COLOR_RESET "\n", t_gpu);
-    img_write(img_name, img_input, img_row, img_col, 1);
+    img_write(img_name, img_output, img_row, img_col, 1, async);
+    // img_write(img_name, img_input, img_row, img_col, 1, async);
+    
+    cuda_err_chk(cudaFree(img_input_cuda), cudaError_cnt++);
+    cuda_err_chk(cudaFree(img_output_cuda), cudaError_cnt++);
+    cuda_err_chk(cudaFree(filter_cuda), cudaError_cnt++);
+    printf("[LOG]: Finished %s\n\n", async ? "async" : "sync");
     
     return t_gpu;
+}
+
+void free_memory(){
+    free(img_input);
+    free(img_output);
+    free(filter);
 }
 
 
 int main(int argc, char* argv[]){
     /*--------------- Init -------------------*/
+    cudaError_cnt = 0;
     if(argc < 2){
         fprintf(stderr, "%s", "Please provide filename for Gaussian Blur. usage ./gb_thread_1D.o <Image file> \n");
         return -1;
     }
     else if(argc == 3){
         sscanf(argv[2], "%d", &thread_cnt);        
-        printf("Testing with %d threads in each CUDA block\n", thread_cnt);
     }
     else{
         // Set default thread count to 1024
         thread_cnt = 1024;
     }
     block_row = (int)sqrt(thread_cnt);
+    
+    int num = 0;
+    cudaGetDeviceCount(&num);
+    cudaDeviceProp prop;
+    if(num > 0){
+        cudaGetDeviceProperties(&prop, 0);
+        printf("[LOG]: Device: %s \n", prop.name);
+    }
+    else{
+        fprintf(stderr, "%s", "No NVIDIA GPU detected!\n");
+        return 1;
+    }
 
     /*---------------- Image and mask IO ----*/
     int img_row, img_col, resolution;
     img_name = argv[1];
     img_input = img_read(img_name, img_row, img_col);
-    resolution = img_row * img_col;
-
+    resolution = 3 * img_row * img_col;
+    
     FILE* mask;
     mask = fopen("mask_Gaussian.txt", "r");
     fscanf(mask, "%d", &filter_size);
     filter_row = (int)sqrt(filter_size);
     filter = new unsigned int [filter_size];
-
+    
     for(int i = 0; i < filter_size; i++){
         fscanf(mask, "%u", &filter[i]);
     }
-
+    
     filter_scale = 0;
     for(int i = 0; i < filter_size; i++){
         filter_scale += filter[i];	
     }
     fclose(mask);
-
     
-    printf("Finished all \n");
+    /*-------------- CUDA run ------------*/
+    int t1, t2;
+    t2 = cuda_run(img_row, img_col, resolution, 1);
+    t1 = cuda_run(img_row, img_col, resolution, 0);
+    
+    printf(ANSI_COLOR_YELLOW "[RESULT]: [img_row, img_col, threads in each block]" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_YELLOW "[RESULT]: [stream workers, speedup ratio]" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_RED ">>> %d, %d, %d" ANSI_COLOR_RESET "\n", img_row, img_col, thread_cnt);
+    printf(ANSI_COLOR_RED ">>> %d, %5.2f" ANSI_COLOR_RESET "\n", N_STREAMS, ((float) t1 / (float)t2));
+    
+    /*-------------- Cleanup ------------*/
+    free_memory();
     return 0;
 }
--- Image_Processing/Gaussian_Blur/utils/img_io.cpp
+++ Image_Processing/Gaussian_Blur/utils/img_io.cpp
@@ -11,23 +11,23 @@ unsigned char* img_read(string img_name, int& img_row, int& img_col){
     Mat img = imread(img_name);
     img_row = img.rows;
     img_col = img.cols;
-    int resolution = img_row * img_col;
+    int resolution = 3 * img_row * img_col;
 
-    unsigned char* ret = new unsigned char [3 * resolution];
+    unsigned char* ret = new unsigned char [resolution];
 
     for(int i = 0; i < img_row; i++){
         for(int j = 0; j < img_col; j++){
             Vec3b color = img.at<Vec3b>(i, j);
-            ret[3 * (i * img_col + j) + 0] = color[2];
-            ret[3 * (i * img_col + j) + 1] = color[1];
-            ret[3 * (i * img_col + j) + 2] = color[0];
+            ret[3 * (i * img_col + j) + 0] = static_cast<unsigned char>(color[2]);
+            ret[3 * (i * img_col + j) + 1] = static_cast<unsigned char>(color[1]);
+            ret[3 * (i * img_col + j) + 2] = static_cast<unsigned char>(color[0]);
         }
     }
 
     return ret;
 }
 
-void img_write(string img_name, unsigned char* img, const int& img_row, const int& img_col, const int thread_dim){
+void img_write(string img_name, unsigned char* img, const int& img_row, const int& img_col, const int& thread_dim, const int& async){
     Mat img_cv(img_row, img_col, CV_8UC3, Scalar(0, 0, 0));
     for (int i = 0; i < img_row; i++){
         for (int j = 0; j < img_col; j++){
@@ -40,6 +40,7 @@ void img_write(string img_name, unsigned char* img, const int& img_row, const in
     }
 
     // string name_imwrite = "./../img_output/" + p.filename() + "_" + to_string(thread_dim) + "D.jpg";
-    string name_imwrite = "out_" + to_string(thread_dim) + "D.jpg";
+    string name_imwrite = "out_" + to_string(thread_dim) + "D_" + ((async) ? "async" : "sync") + ".jpg";
     assert(imwrite(name_imwrite, img_cv) == true);
+    printf("[LOG]: img_writie() finished\n");
 }
