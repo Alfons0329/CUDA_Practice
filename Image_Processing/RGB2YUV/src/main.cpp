c89b1b510af298d6b8481ba38741fa64a4073fd7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-11-26 15:18:49 +0800

    New, general makefile

--- Image_Processing/RGB2YUV/src/main.cpp
+++ Image_Processing/RGB2YUV/src/main.cpp
@@ -1,7 +1,14 @@
 #include <cuda_runtime_api.h>
 #include "nvjpegDecoder.h"
+#include <bits/stdc++.h>
 
 using namespace std;
+
+void img_read_nv(string img_name, int &img_row, int &img_col){
+}
+
+void img_write_nv(string img_name, unsigned char *img, const int &img_row, const int &img_col, const int &thread_dim, const int &async){
+}
 
 int main(){
     decode_params_t params;
@@ -9,11 +16,11 @@ int main(){
     nvjpegPinnedAllocator_t pinned_allocator = {&host_malloc, &host_free};
     nvjpegStatus_t status = nvjpegCreateEx(NVJPEG_BACKEND_HARDWARE, &dev_allocator, &pinned_allocator, NVJPEG_FLAGS_DEFAULT, &params.nvjpeg_handle);
 
-
     cudaEvent_t ev;
     cudaEventCreate(&ev);
     cudaEventCreateWithFlags(&ev, cudaEventBlockingSync);
     cout << "Name: " << typeid(decltype(params)).name()  << "\n";
     cout << "Name: " << typeid(decltype(ev)).name() << "\n";
+    cout << "Finished testing " << "\n";
     return 0;
 }
